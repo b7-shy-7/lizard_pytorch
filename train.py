258dc4ae0dea1fe6707b10d14d6952dabd0c2a50
PC <2368458361@qq.com>
2023-11-22 19:42:13 +0800

    11.22

--- train.py
+++ train.py
@@ -4,14 +4,17 @@ import torch.optim as optim
 import torchvision.transforms as transform
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
+import torchvision
 from torchvision.datasets import FashionMNIST
-from torch.
+from torch.utils.tensorboard import SummaryWriter
+
+from tqdm import tqdm
 
 from network import Net
 
 batch_size = 100
 
-data = FashionMNIST(
+dataset = FashionMNIST(
     root = './data',
     train = True,
     transform = transform.Compose([
@@ -20,18 +23,28 @@ data = FashionMNIST(
     download = True
 )
 
-net = Net(batch_size=batch_size).cuda()
+net = Net(batch_size=batch_size)
 
-loader = DataLoader(data, batch_size = batch_size)
+loader = DataLoader(dataset, batch_size = batch_size)
 optimizer = optim.Adam(net.parameters(), lr = 0.01)
-loss = nn.CrossEntropyLoss().cuda()
+loss = nn.CrossEntropyLoss()
 
 correct_number = 0
 
-for i in range(10):
-    for data, label in loader:
-        data = data.cuda()
-        label = label.cuda()
+tb  = SummaryWriter()
+images, labels = next(iter(loader))
+grid = torchvision.utils.make_grid(images)
+
+tb.add_image('images', grid)
+tb.add_graph(net, images)
+# tb.close()
+
+for epoch in range(10):
+    total_loss = 0
+    total_correct = 0
+    for data, label in tqdm(loader, desc = "Training Epoch %d: ".format(epoch)):
+        # data = data.cuda()
+        # label = label.cuda()
         output = net(data)
         label = label
         # output = torch.argmax(output, dim = 1)
@@ -41,7 +54,19 @@ for i in range(10):
         loss.backward()
         optimizer.step()
         correct_number += torch.argmax(output, dim = 1).eq(label).sum(dim = 0)
+
+        total_loss += loss.item()
+        total_correct = correct_number
         # print(loss)
-    print(correct_number.item())
-    correct_number = 0
+    
+    tb.add_scalar('Loss', total_loss, epoch)
+    tb.add_scalar('Number Correct', total_correct, epoch)
+    tb.add_scalar('Accuracy', correct_number.item() / len(dataset), epoch)
+
+    tb.add_histogram('conv1.bias', net.conv1.bias, epoch)
+    tb.add_histogram('conv.weight', net.conv1.weight, epoch)
+    tb.add_histogram('conv.weight.grad', net.conv1.weight.grad, epoch)
+
+    # print(correct_number.item())
+    # correct_number = 0
 
